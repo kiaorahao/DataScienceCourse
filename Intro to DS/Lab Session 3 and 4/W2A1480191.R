5a64f87317b047151bc7ec5edc4d2b0b6cb7449e
Harry <Harry@Harrys-Mac-mini.local>
2017-11-20 12:16:26 +1300

    lable34
    lable34

--- Intro to DS/Lab Session 3 and 4/W2A1480191.R
+++ Intro to DS/Lab Session 3 and 4/W2A1480191.R
@@ -1,16 +1,48 @@
-# 1. Omitting all the transactions made before 1 Jan 2010
+# 0. raw data cleaning
+rm(list=ls())
+cat("\14")
 library(readr)
-table1 <- read_csv("~/Documents/OneDrive/PGD/Data Science/Lab Session 3 and 4/Datasets/Data Set 1a.csv")
-table2 <- read_csv("~/Documents/OneDrive/PGD/Data Science/Lab Session 3 and 4/Datasets/Data Set 1b.csv")
+table1 <- read_csv("~/Data Science/Datasets/Data Set 1a.csv")
+table2 <- read_csv("~/Data Science/Datasets/Data Set 1b.csv")
+
+levels(table1$Product)	<- c(levels(table1$Product),	"Galaxy Edge S8")
+table1$Product[table1$Product	== "Galaxi Edge S8"]	<- "Galaxy Edge S8"
+
+table1$Product[table1$Product	=="ALice"]<-"Alice"
+names(table1)[names(table1)=='Name']<-'Customer'
+
+table1$Quantity[table1$Quantity=="one"]<-"1"
+table1$Quantity[table1$Quantity=="two"]<-"2"
+
+library(lubridate)
+table1$Date = dmy(table1$Date)
+
 table3 = merge(table1,table2,"Product")
-table4 = subset(table3,table3$Date>'2017-01-01')
+
+Calculate.Rate <- function(arg1){	
+  if	(arg1=='NZD')	{out<-1}	
+  if	(arg1=='AUD')	{out<-1.2}		
+  return(out)
+}
+table3["Exchange.Rate"]<-sapply(table3$Currency,Calculate.Rate)
+table3$Unit.Price.NZD<-table3$`Unit Price`*table3$Exchange.Rate
+#  table3$Unit.Price.NZD = as.numeric(table3$Unit.Price.NZD)
+
+# 1. Omitting all the transactions made before 1 Jan 2010
+
+
+table4 = subset(table3,table3$Date>'2010-01-01')
 
 # 2. Calculating monthly total sale
 library(zoo)
+library(plyr)
+
 table4$YearMon=as.yearmon(table4$Date)
 GroupColumns = c('YearMon')
-DataColumns=c('Quantity','Unit.Price.NZD')
-res=ddply(table4,GroupColumns,function(x) colSums(x[DataColumns]))
+DataColumns = c('Quantity','Unit.Price.NZD')
+#DataColumns=c('Unit.Price.NZD')
+#res = ddply(table4,GroupColumns,function(x) colSums(x[DataColumns]))
+res = ddply(table4,GroupColumns,function(x) colSums(x[DataColumns]))
 table5=head(res)
 
 # 3. Visualizing the results (monthly total sale) by using appropriate charts/plots.
@@ -31,3 +63,4 @@ p_Prize = plot_ly(
   name = 'Prize',
   type = 'bar'
 )
+
